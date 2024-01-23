915549ba3e0d6d929abacb4fb5ebb07572402009
Souji Thenria <mail@souji-thenria.net>
2024-01-23 10:57:06 +0000

    Rework the cli tooling

--- config.go
+++ config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/charmbracelet/log"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -37,28 +36,11 @@ func config() (config map[string]any, err error) {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			log.Warn("No configuration found, using defaults")
-			// log.Info("Write configuration")
-			// // TODO: does not create directory
-			// // TODO: does not resolve $HOME
-			// err = viper.SafeWriteConfigAs("/home/souji/.config/myip/conf.toml")
-			// if err != nil {
-			//     log.Fatal("Failed to write config", "error", err)
-			// }
+            // TODO: create new config file
 		} else {
             return nil, err
 		}
 	}
-
-    pflag.String(CONF_CA_CERT, "", "Path to the root CA certificate")
-    pflag.String(CONF_CERT_FILE, "", "Path to the certificate")
-    pflag.String(CONF_KEY_FILE, "", "Path to the key file")
-    pflag.String(CONF_LISTEN, "", "Address the server listens on")
-    pflag.Int16P(CONF_PORT, "p", 9001, "The port to use")
-    pflag.String(CONF_SERVER, "", "The server the client connects to")
-    pflag.BoolP(CONF_RUN_SERVER, "s", false, "Run the server")
-    pflag.BoolP(CONF_RUN_CLIENT, "c", false, "Run the client")
-    pflag.Parse()
-    viper.BindPFlags(pflag.CommandLine)
 
     // Yeah, thats an ugly hack
     viper.Set(CONF_PORT, viper.GetInt(CONF_PORT))
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"os"
+
 	"github.com/charmbracelet/log"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
@@ -10,22 +13,93 @@ func main() {
         log.Fatal("Failed to read config", "error", err)
     }
 
-	if c[CONF_RUN_SERVER].(bool) {
-		server(&ServerConfig{
-			CaCertPath: c[CONF_CA_CERT].(string),
-			CertFile:   c[CONF_CERT_FILE].(string),
-			KeyFile:    c[CONF_KEY_FILE].(string),
-			Port:       uint16(c[CONF_PORT].(int)),
-			Server:     c[CONF_LISTEN].(string),
-		})
-		return
-	}
+    app := &cli.App {
+        Name: "myip",
+        Usage: "The client application",
+        Flags: []cli.Flag{
+            &cli.StringFlag{
+                Name: "ca",
+                Value: c[CONF_CA_CERT].(string),
+                Usage: "Path to the root CA file",
+            },
+            &cli.StringFlag{
+                Name: "crt",
+                Value: c[CONF_CERT_FILE].(string),
+                Usage: "Path to the server certificate",
+            },
+            &cli.StringFlag{
+                Name: "key",
+                Value: c[CONF_KEY_FILE].(string),
+                Usage: "Path to the server key",
+            },
+            &cli.IntFlag{
+                Name: "port",
+                Value: c[CONF_PORT].(int),
+                Usage: "The port the server listens on",
+            },
+            &cli.StringFlag{
+                Name: "server",
+                Value: c[CONF_SERVER].(string),
+                Usage: "The IP or DNS of the server",
+            },
+        },
+        Action: func(cCtx *cli.Context) error {
+            client(&ClientConfig{
+                CaCertPath: cCtx.String("ca"), 
+                CertFile:   cCtx.String("crt"), 
+                KeyFile:    cCtx.String("key"), 
+                Port:       uint16(cCtx.Int("port")), 
+                Server:     cCtx.String("server"), 
+            })
+            return nil
+        },
 
-	client(&ClientConfig{
-        CaCertPath: c[CONF_CA_CERT].(string),
-        CertFile:   c[CONF_CERT_FILE].(string),
-        KeyFile:    c[CONF_KEY_FILE].(string),
-        Port:       uint16(c[CONF_PORT].(int)),
-        Server:     c[CONF_SERVER].(string),
-    })
+        Commands: []*cli.Command{
+            {
+                Name: "server",
+                Usage: "Start the server",
+                Flags: []cli.Flag{
+                    &cli.StringFlag{
+                        Name: "ca",
+                        Value: c[CONF_CA_CERT].(string),
+                        Usage: "Path to the root CA file",
+                    },
+                    &cli.StringFlag{
+                        Name: "crt",
+                        Value: c[CONF_CERT_FILE].(string),
+                        Usage: "Path to the server certificate",
+                    },
+                    &cli.StringFlag{
+                        Name: "key",
+                        Value: c[CONF_KEY_FILE].(string),
+                        Usage: "Path to the server key",
+                    },
+                    &cli.IntFlag{
+                        Name: "port",
+                        Value: c[CONF_PORT].(int),
+                        Usage: "The port the server listens on",
+                    },
+                    &cli.StringFlag{
+                        Name: "listen",
+                        Value: c[CONF_LISTEN].(string),
+                        Usage: "The IP the server listen on",
+                    },
+                },
+                Action: func(cCtx *cli.Context) error {
+                    server(&ServerConfig{
+                        CaCertPath: cCtx.String("ca"), 
+                        CertFile:   cCtx.String("crt"), 
+                        KeyFile:    cCtx.String("key"), 
+                        Port:       uint16(cCtx.Int("port")), 
+                        Server:     cCtx.String("listen"), 
+                    })
+                    return nil
+                },
+            },
+        },
+    }
+
+    if err := app.Run(os.Args); err != nil {
+        log.Fatal("Failed to execute", "error", err)
+    }
 }
