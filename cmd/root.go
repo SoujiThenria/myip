7491081d53310ee38a6b6be48a7486975dd21fff
Souji Thenria <mail@souji-thenria.net>
2024-01-25 01:29:55 +0000

    Improve cobra commands

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,103 @@
 package cmd
 
 import (
-    "github.com/charmbracelet/log"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
 
-type ClientOptions struct {
-}
-
-var rootCmd = &cobra.Command{
-  Use:   "myip",
-  Short: "The client side of the myip utility",
-  Version: "0.1-beta",
-  Run: func(cmd *cobra.Command, args []string) {
-      log.Info("Start client")
-  },
-}
+var (
+    c *ClientConfig
+    rootCmd = &cobra.Command{
+        Use:   "myip",
+        Short: "The client side of the myip utility",
+        Version: "0.1-beta",
+        Run: func(cmd *cobra.Command, args []string) {
+            log.Info("Start client")
+            client(c)
+        },
+    }
+)
 
 func Execute() {
+    c = new(ClientConfig)
+
+    rootCmd.Flags().StringVar(&c.CaCertPath, "ca", "/etc/ssl/server.crt", "Certificate of the root CA")
+    rootCmd.Flags().StringVar(&c.CertFile, "crt", "$HOME/.config/myip/client.crt", "Client certificate")
+    rootCmd.Flags().StringVar(&c.KeyFile, "key", "$HOME/.config/myip/client.key", "Client key")
+    rootCmd.Flags().StringVar(&c.Server, "server", "localhost", "IP or DNS name of the server")
+    rootCmd.Flags().Uint16Var(&c.Port, "port", 443, "The port to use")
+
+    rootCmd.AddCommand(newServerCommand())
+
   if err := rootCmd.Execute(); err != nil {
       log.Fatal("Failed to execute", "error", err)
   }
+}
+
+type ClientConfig struct {
+	CaCertPath string
+	CertFile   string
+	KeyFile    string
+	Port       uint16
+	Server     string
+}
+
+func client(c *ClientConfig) {
+	caCert, err := os.ReadFile(c.CaCertPath)
+	if err != nil {
+		log.Fatal("Failed to read certificate", "error", err)
+	}
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(caCert) {
+		log.Warn("No certificates where parsed")
+	}
+
+	cert, err := tls.LoadX509KeyPair(c.CertFile, c.KeyFile)
+	if err != nil {
+		log.Fatal("Failed to load the client certificate and key", "error", err)
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:      caCertPool,
+				Certificates: []tls.Certificate{cert},
+			},
+		},
+	}
+
+	resp, err := client.Get("https://" + buildAddress(c.Server, c.Port))
+	if err != nil {
+		log.Fatal("Failed to connect to the server", "error", err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Warn("Failed to close response body", "error", err)
+		}
+	}()
+
+	respByte, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Failed to read response body", "error", err)
+	}
+	fmt.Println(string(respByte))
+}
+
+func buildAddress(server string, port uint16) string {
+	return server + ":" + strconv.Itoa(int(port))
+}
+
+func getFlag(flag string, err error) string {
+    if err != nil {
+        log.Fatal(err)
+    }
+    return flag
 }
--- cmd/server.go
+++ cmd/server.go
@@ -5,14 +5,34 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-    rootCmd.AddCommand(serverCmd)
+type ServerOptions struct {
+    RootCA string
+    ClientCrt string
+    ClientKey string
+    Port uint16
+    Listen string
 }
 
-var serverCmd = &cobra.Command{
-  Use:   "server",
-  Short: "The server side of the myip utility",
-  Run: func(cmd *cobra.Command, args []string) {
-      log.Info("Start the server")
-  },
+func newServerCommand() *cobra.Command {
+    opts := new(ServerOptions)
+    serverCmd := &cobra.Command{
+        Use:   "server",
+        Short: "The server side of the myip utility",
+        Run: func(cmd *cobra.Command, args []string) {
+            log.Info("Start the server")
+            server(opts)
+        },
+    }
+
+    serverCmd.Flags().StringVar(&opts.RootCA, "ca", "/etc/ssl/server.crt", "Certificate of the root CA")
+    serverCmd.Flags().StringVar(&opts.ClientCrt, "crt", "/usr/local/etc/myip/server.crt", "Client certificate")
+    serverCmd.Flags().StringVar(&opts.ClientKey, "key", "/usr/local/etc/myip/server.key", "Client key")
+    serverCmd.Flags().StringVar(&opts.Listen, "listen", "0.0.0.0", "The address the server binds on")
+    serverCmd.Flags().Uint16Var(&opts.Port, "port", 443, "The port to use")
+
+    return serverCmd
+}
+
+func server(c *ServerOptions) {
+    log.Info(c)
 }
