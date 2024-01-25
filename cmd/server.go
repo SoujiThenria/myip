dff10656f2a679ed09d625ac79b0a7aff6866e78
Souji Thenria <mail@souji-thenria.net>
2024-01-25 01:41:51 +0000

    Implement server functionality

--- cmd/server.go
+++ cmd/server.go
@@ -1,14 +1,20 @@
 package cmd
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"net/http"
+	"os"
+	"strings"
+
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
 
 type ServerOptions struct {
     RootCA string
-    ClientCrt string
-    ClientKey string
+    ServerCrt string
+    ServerKey string
     Port uint16
     Listen string
 }
@@ -25,8 +31,8 @@ func newServerCommand() *cobra.Command {
     }
 
     serverCmd.Flags().StringVar(&opts.RootCA, "ca", "/etc/ssl/server.crt", "Certificate of the root CA")
-    serverCmd.Flags().StringVar(&opts.ClientCrt, "crt", "/usr/local/etc/myip/server.crt", "Client certificate")
-    serverCmd.Flags().StringVar(&opts.ClientKey, "key", "/usr/local/etc/myip/server.key", "Client key")
+    serverCmd.Flags().StringVar(&opts.ServerCrt, "crt", "/usr/local/etc/myip/server.crt", "Client certificate")
+    serverCmd.Flags().StringVar(&opts.ServerKey, "key", "/usr/local/etc/myip/server.key", "Client key")
     serverCmd.Flags().StringVar(&opts.Listen, "listen", "0.0.0.0", "The address the server binds on")
     serverCmd.Flags().Uint16Var(&opts.Port, "port", 443, "The port to use")
 
@@ -34,5 +40,34 @@ func newServerCommand() *cobra.Command {
 }
 
 func server(c *ServerOptions) {
-    log.Info(c)
+	caCert, err := os.ReadFile(c.RootCA)
+	if err != nil {
+		log.Fatal("Failed to read certificate", "error", err)
+	}
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(caCert) {
+		log.Warn("No certificates where parsed")
+	}
+
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+
+	server := &http.Server{
+		Addr:      buildAddress(c.Listen, c.Port),
+		TLSConfig: tlsConfig,
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		clientAddress := r.RemoteAddr
+		log.Info("New connection", "client", clientAddress)
+		w.Write([]byte(clientAddress[:strings.LastIndex(clientAddress, ":")]))
+	})
+
+	log.Info("Start server", "server", c.Listen, "port", c.Port)
+	if err := server.ListenAndServeTLS(c.ServerCrt, c.ServerKey); err != nil {
+		log.Fatal("Failed to start the server", "error", err)
+	}
 }
